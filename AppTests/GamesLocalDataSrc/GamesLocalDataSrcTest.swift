ef55ed612205097cf7a1c1f16de9aa3d7655ac4f
Shaban <sh3ban.kamel@gmail.com>
2023-06-04 17:38:32 +0300

    Test GamesLocalDataSrc

--- AppTests/GamesLocalDataSrc/GamesLocalDataSrcTest.swift
+++ AppTests/GamesLocalDataSrc/GamesLocalDataSrcTest.swift
@@ -20,4 +20,140 @@ class GamesLocalDataSrcTest: XCTestCase {
         subject = nil
         super.tearDown()
     }
+
+    func testLastGameResponse() {
+        // Given
+        let gamesResponse = GamesResponse(
+                next: "2",
+                previous: "",
+                results: [],
+                seoTitle: "",
+                seoDescription: "",
+                seoKeywords: "",
+                seoH1: "",
+                noindex: false,
+                nofollow: false,
+                description: "",
+                nofollowCollections: []
+        )
+
+        cacheManager.getReturnValue = gamesResponse
+
+        runAsyncTest { [self] in
+            // When
+            let result = try await subject.lastGameResponse()
+            // Then
+            XCTAssertEqual(result?.next, gamesResponse.next)
+        }
+    }
+
+    func testGames() {
+        // Given
+        let gameItemResponse1 = GameItemResponse(
+                id: 1,
+                name: "Test Game",
+                backgroundImage: nil,
+                metacritic: nil,
+                genres: [])
+        let gameItemResponse2 = GameItemResponse(
+                id: 1,
+                name: "Test Game",
+                backgroundImage: nil,
+                metacritic: nil,
+                genres: [])
+        let gameItemResponseArray = [gameItemResponse1, gameItemResponse2]
+
+        cacheManager.arrayReturnValue = gameItemResponseArray
+
+        runAsyncTest { [self] in
+            // When
+            let result = try await subject.games()
+            // Then
+            XCTAssertEqual(result.count, gameItemResponseArray.count)
+        }
+    }
+
+    func testCache() {
+        // Given
+        let gamesResponse = GamesResponse(
+                next: "2",
+                previous: "",
+                results: [],
+                seoTitle: "",
+                seoDescription: "",
+                seoKeywords: "",
+                seoH1: "",
+                noindex: false,
+                nofollow: false,
+                description: "",
+                nofollowCollections: []
+        )
+
+        runAsyncTest { [self] in
+            // When
+            try await subject.cache(gamesResponse: gamesResponse)
+
+            // Then
+            XCTAssertTrue(cacheManager.saveCalled)
+            XCTAssertTrue(cacheManager.appendCalled)
+        }
+    }
+
+    func testClearCache() {
+        runAsyncTest { [self] in
+            try await subject.clearCache()
+            XCTAssertTrue(cacheManager.removeCalled)
+        }
+    }
+
+    func testUpdateFavorite() {
+        cacheManager.arrayReturnValue = []
+        // Given
+        let gameItemResponse = GameItemResponse(
+                id: 1,
+                name: "Test Game",
+                backgroundImage: nil,
+                metacritic: nil,
+                genres: [])
+
+        runAsyncTest { [self] in
+            // When
+            let result1 = try await subject.updateFavorite(item: gameItemResponse)
+
+            // When
+            cacheManager.arrayReturnValue = [gameItemResponse]
+            let result2 = try await subject.updateFavorite(item: gameItemResponse)
+
+            // Then
+            XCTAssertTrue(result1)
+            XCTAssertFalse(result2)
+        }
+    }
+
+    func testFavorites() {
+        let gameItemResponse1 = GameItemResponse(
+                id: 1,
+                name: "Test Game",
+                backgroundImage: nil,
+                metacritic: nil,
+                genres: [])
+        let gameItemResponse2 = GameItemResponse(
+                id: 1,
+                name: "Test Game",
+                backgroundImage: nil,
+                metacritic: nil,
+                genres: [])
+        let gameItemResponseArray = [gameItemResponse1, gameItemResponse2]
+
+        // Given
+        cacheManager.arrayReturnValue = gameItemResponseArray
+
+        runAsyncTest { [self] in
+            // When
+            let result = try await subject.favorites()
+
+            // Then
+            XCTAssertEqual(result.count, gameItemResponseArray.count)
+        }
+    }
 }
--- AppTests/Shared/XCTestCase+RunAsyncTest.swift
+++ AppTests/Shared/XCTestCase+RunAsyncTest.swift
@@ -19,4 +19,39 @@ extension XCTestCase {
         }
         waitForExpectations(timeout: timeout, handler: nil)
     }
+
+}
+
+extension XCTestCase {
+    func runAsyncTest(
+            named testName: String = #function,
+            in file: StaticString = #file,
+            at line: UInt = #line,
+            withTimeout timeout: TimeInterval = 10,
+            test: @escaping () async throws -> Void
+    ) {
+        var thrownError: Error?
+        let errorHandler = { thrownError = $0 }
+        let expectation = expectation(description: testName)
+
+        Task {
+            do {
+                try await test()
+            } catch {
+                errorHandler(error)
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: timeout)
+
+        if let error = thrownError {
+            XCTFail(
+                    "Async error thrown: \(error)",
+                    file: file,
+                    line: line
+            )
+        }
+    }
 }
